19a8ba7417693d4687d5a75777a8fd4968366953
Aifert Yet <aifertyet0628@gmail.com>
2024-04-25 14:35:18 +0800

    implemented the board, integrate a leaderboard now

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('/pages/index.html')
+    return render_template('/pages/index.html', css_file_path='/static/index_style.css')
 
 @app.route('/login')
 def login():
