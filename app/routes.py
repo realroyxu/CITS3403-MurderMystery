32ee2a40d71995df475ea49c796584f51930ae9b
Aifert Yet <aifertyet0628@gmail.com>
2024-04-25 11:44:44 +0800

    made routes, add html files next

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,24 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('/pages/index.html')
+
+@app.route('/login')
+def login():
+    return
+
+@app.route('/register')
+def register():
+    return
+
+@app.route('/leaderboard')
+def leaderboard():
+    return
+
+@app.route('/forum/{id}')
+def forum(id):
+    return
+
+
+@app.route('/createForum/{id}')
+def createSudoku(id):
+    return
