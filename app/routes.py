4ef599ecf71e9fbf8bbacf4ef65ed5090b44565c
Aifert Yet <aifertyet0628@gmail.com>
2024-05-03 20:26:45 +0800

    fix forum bug on home page

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,7 @@ user_scores = [
 ]
 
 forum_posts = [
-    {"id": 1, "title": "Post 1", "content": "This is the content of post 1."},
+    {"id": 1, "title": "Post 1", "content": "This is the content of post 1. This is the content of post 1.This is the content of post 1. This is the content of post 1. This is the content of post 1. v v v This is the content of post 1. This is the content of post 1. v "},
     {"id": 2, "title": "Post 2", "content": "This is the content of post 2."},
     {"id": 3, "title": "Post 3", "content": "This is the content of post 3."},
     {"id": 4, "title": "Post 4", "content": "This is the content of post 4."},
