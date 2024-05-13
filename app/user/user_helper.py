b2faa9e31f77ffa3bd870481bfa57fd34c17623d
Aifert Yet <aifertyet0628@gmail.com>
2024-05-13 22:10:01 +0800

    refactor : change user helper to class based services

--- app/user/user_helper.py
+++ app/user/user_helper.py
@@ -1,27 +1,39 @@
 import app.user.user_db_helper as User_DB
-import db.db_error_helper as ERROR
 from app.models.user import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
+class UserService:
+    def hash_password(self, password):
+        """Hash a password for storing."""
+        password_hash = generate_password_hash(password)
+        return password_hash
 
-def check_password(db_user_password, provided_password):
-    """Check password hash"""
-    return db_user_password == provided_password
+    def check_password(self, hashed_password, given_password):
+        """Check hashed password."""
+        return check_password_hash(hashed_password, given_password)
 
-
-def authenticate_user(username, password):
-    """Authenticate user on login"""
-    try:
+    def authenticate_user(self, username, password):
+        """Authenticate user on login"""
         user = User_DB.get_user(User, {'username': username})
-    except ERROR.DB_Error as e:
-        raise ERROR.DB_Error(str(e))
-    return check_password(user.password, password)
+        return self.check_password(user.password, password)
+
+    def change_password(self, username, old_password, new_password):
+        """Change password for user"""
+        user = User_DB.get_user(User, {'username': username})
+        if self.check_password(user.password, old_password):
+            user.password = self.hash_password(new_password)
+            User_DB.add_user(User, user)
+            return True
+        return False
+
+    def register_user(self, username, password, email="hello_world@gmail.com", avatar="hello"):
+        data = {
+            "username": username,
+            "password": self.hash_password(password),
+            "email": email,
+            "avatar": avatar
+        }
+        return User_DB.add_user(User, data)
 
 
-def register_user(username, password, email="hello_world@gmail.com", avatar="hello"):
-    data = {
-        "username": username,
-        "password": password,
-        "email": email,
-        "avatar": avatar
-    }
-    return User_DB.add_user(User, data)
+user_service = UserService()
