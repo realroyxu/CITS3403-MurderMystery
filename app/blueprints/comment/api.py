c9988ae164cb4249f723447c447324beff0da98a
Roy Xu <realroyxu@icloud.com>
2024-05-17 11:27:08 +0800

    comment functions now

--- app/blueprints/comment/api.py
+++ app/blueprints/comment/api.py
@@ -1,3 +1,4 @@
+
 from . import comment_bp
 from . import comment_helper
 from db.db_error_helper import DB_Error
@@ -5,11 +6,13 @@ from flask import request, jsonify, session
 
 
 @comment_bp.route('/api/comment/<int:id>', methods=['POST'])
+# 'comment' here is a verb
 # need [postid, commenttext]
 # [userid] will be taken from session
 def add_comment(id):
     data = request.get_json()
-    del data['csrf_token']
+    if 'csrf_token' in data:
+        del data['csrf_token']
     data['userid'] = session['userid']
     data['postid'] = id
     try:
@@ -40,3 +43,13 @@ def delete_comment():
         return jsonify({"message": "Comment deleted successfully"}), 200
     except DB_Error as e:
         return jsonify({"message": f"Error deleting comment: {e}"}), 401
+
+
+@comment_bp.route('/api/getcomment/<int:id>', methods=['POST'])
+# not completed yet
+def get_comment(id):
+    try:
+        comment_helper.get_comments({'postid': id})
+        return jsonify({"message": "Comment get successfully"}), 200
+    except DB_Error as e:
+        return jsonify({"message": f"Error getting comment: {e}"}), 401
