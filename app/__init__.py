308c6326f87e0f5af4139d2675d03cb4cda8b8af
Aifert Yet <aifertyet0628@gmail.com>
2024-05-18 15:04:39 +0800

    add alembic migrations

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,17 @@ from app.blueprints.comment import comment_bp
 from app.blueprints.attempt import attempt_bp
 from app.blueprints.leaderboard import siteleaderboard_bp, postleaderboard_bp
 from app.blueprints.failure import failure_bp
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'abcde'
     app.config['UPLOAD_FOLDER'] = 'app/static/uploads/'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/ormtest.db'
+
+    db.init_app(app)
 
     app.register_blueprint(index_bp)
     app.register_blueprint(user_api_bp)
