38f73e705a3ee78ab066d96f936158c9a10deeb4
Aifert Yet <aifertyet0628@gmail.com>
2024-05-19 15:05:39 +0800

    site leaderboard api test

--- app/blueprints/leaderboard/api.py
+++ app/blueprints/leaderboard/api.py
@@ -30,27 +30,3 @@ def get_siteleaderboard_solvecount_order():
         return jsonify(data), 200
     except ERROR.DB_Error as e:
         return jsonify({"message": f"Error getting siteleaderboard: {e}"}), 401
-
-
-@postleaderboard_bp.route('/api/getplb', methods=['POST'])
-def get_postleaderboard():
-    # need [postid]
-    data = request.get_json()
-    try:
-        return jsonify(plb_helper.get_plb(data)), 200
-    except ERROR.DB_Error as e:
-        return jsonify({"message": f"Error getting postleaderboard: {e}"}), 401
-
-
-# ISSUE: THIS IS A PUBLIC API AND THUS CAN BE EXPLOITED, NEED EXTRA VALIDATION OF PUZZLE SOLVING
-@postleaderboard_bp.route('/api/addplb', methods=['POST'])
-def add_postleaderboard():
-    # need [postid]
-    # [userid] will be taken from session
-    data = request.get_json()
-    try:
-        data['userid'] = session['userid']
-        plb_helper.add_record(data)
-        return jsonify({"message": "Postleaderboard added successfully"}), 200
-    except ERROR.DB_Error as e:
-        return jsonify({"message": f"Error adding postleaderboard: {e}"}), 401
